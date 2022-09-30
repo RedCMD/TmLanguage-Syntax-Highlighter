13c7261575e78db553206e55a55958667a35d188
RedCMD <theredcmd@gmail.com>
2022-09-30 17:46:58 +1300

    Improve `"include"` code suggestions

--- out/CompletionItemProvider.js
+++ out/CompletionItemProvider.js
@@ -3,87 +3,131 @@ const _extension = require("./extension.js");
 
 const CompletionItemProvider = {
 	async provideCompletionItems(document, position, token, context) {
-		// const { getNodeAtLocation } = await extension.parseTreeExtension.activate()
-		// const location = new vscode.Location(document.uri, position)
-		// const node = getNodeAtLocation(location)
 		const { getTree } = await _extension.parseTreeExtension.activate()
 		const tree = getTree(document)
 		const node = _extension.getNodeAtPosition(tree.rootNode, position)
 		const completions = []
 		// vscode.window.showInformationMessage(JSON.stringify(node.text))
 
-		// if (node.previousNamedSibling.text == '')
-		// node = node.previousNamedSibling
-		// vscode.window.showInformationMessage(JSON.stringify(node.type))
-		// vscode.window.showInformationMessage(JSON.stringify(node.parent.type))
 
-		// if (node.type == 'value' && node.parent.type == 'include') { 
-		
 		// 0 width nodes in tree-sitter are very buggy
 		if (node.type == 'value' && (node.parent.type == 'include' || node.parent.parent.type == 'include')) {
-		// if ((node.type == 'value' || node.previousSibling.type == 'value') && node.parent.type == 'include') {
-			// const { getTree } = await extension.parseTreeExtension.activate()
-			// const tree = getTree(document)
-			// vscode.window.showInformationMessage(JSON.stringify(tree.rootNode.type))
+			const text = node.text
+			// TODO: move this to tree-sitter grammar
+			const indexOfHash = text.indexOf("#")
+			const includeText = text.substring(indexOfHash + 1)
+			const nameScope = indexOfHash == -1 ? text : text.substring(0, indexOfHash)
+			// 0 width nodes in tree-sitter are very buggy
+			const range = _extension.nodeToVscodeRange(node.type == 'value' ? node : node.previousSibling)
 
-			tree.rootNode.namedChildren.forEach(rootNode => {
+
+			for (const rootNode of tree.rootNode.namedChildren) {
 				if (rootNode.type != 'repository')
-					return
+					continue
 
-				rootNode.namedChildren.forEach(repoNode => {
+				for (const repoNode of rootNode.namedChildren) {
 					if (repoNode.type != 'repo')
-						return
+						continue
 					
 					const firstNamedChild = repoNode.firstNamedChild
 					if (firstNamedChild == null)
-						return
+						continue
+					
 
 					const label = '#' + firstNamedChild.text
-																// 0 width nodes in tree-sitter are very buggy
-					const range = _extension.nodeToVscodeRange(node.type == 'value' ? node : node.previousSibling)
 					const completion = {
 						label: label,
 						range: range,
 						kind: vscode.CompletionItemKind.Function
 					}
+					
+					for (const commentNode of repoNode.namedChildren) {
+						if (commentNode.type != 'comment')
+							continue
+						
+						if (commentNode.namedChild(1) == null)
+							continue
+						
+						completion.detail = commentNode.namedChild(1).text
+						break
+					}
+					
 					completions.push(completion)
-				})
-			})
+				}
+			}
 
 
-			vscode.extensions.all.forEach(extension => {
+
+			for (const extension of vscode.extensions.all) {
 				const packageJSON = extension.packageJSON
 				if (packageJSON == null)
-					return
+					continue
 
 				const contributes = packageJSON.contributes
 				if (contributes == null)
-					return
+					continue
 
 				const grammars = contributes.grammars
 				if (grammars == null)
-					return
+					continue
 
-				grammars.forEach(grammar => {
+				for (const grammar of grammars) {
 					const scopeName = grammar.scopeName
 					if (scopeName == null)
-						return
+						continue
 
-																// 0 width nodes in tree-sitter are very buggy
-					const range = _extension.nodeToVscodeRange(node.type == 'value' ? node : node.previousSibling)
+					// 0 width nodes in tree-sitter are very buggy
 					const completion = {
 						label: scopeName,
 						range: range,
 						kind: vscode.CompletionItemKind.Field
 					}
-					
+					const language = grammar.language
+					if (language != null)
+						completion.detail = language
 					completions.push(completion)
-				})
-			})
+
+					if (nameScope == scopeName) {
+						const path = vscode.Uri.joinPath(extension.extensionUri, grammar.path)
+						const textDocument = await vscode.workspace.openTextDocument(path)
+						const documentTree = getTree(textDocument)
+
+						if (documentTree.rootNode.namedChildren.some(rootNode =>
+							rootNode.type == 'scopeName' && rootNode.namedChild(1).text == nameScope
+						)) {
+							for (const rootNode of documentTree.rootNode.namedChildren) {
+								if (rootNode.type != 'repository')
+									continue
+
+								for (const repoNode of rootNode.namedChildren) {
+									if (repoNode.type != 'repo')
+										continue
+
+									if (repoNode.namedChild(0) == null)
+										continue
+
+									const completion = {
+										label: nameScope + '#' + repoNode.namedChild(0).text,
+										range: range,
+										kind: vscode.CompletionItemKind.Field
+									}
+									completions.push(completion)
+								}
+							}
+						}
+					}
+				}
+			}
 
 
-			completions.push(new vscode.CompletionItem('$self', vscode.CompletionItemKind.Class))
-			completions.push(new vscode.CompletionItem('$base', vscode.CompletionItemKind.Class))
+			completions.push(new vscode.CompletionItem(
+				{ label: '$self', description: 'Includes the current grammar file' },
+				vscode.CompletionItemKind.Class
+			))
+			completions.push(new vscode.CompletionItem(
+				{ label: '$base', description: 'Includes the highest parent grammar' },
+				vscode.CompletionItemKind.Class
+			))
 
 
 			// completions.push(new vscode.CompletionItem('#$$$'))
