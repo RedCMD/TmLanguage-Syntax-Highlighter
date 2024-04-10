288b640856e258940b8964e6cbbb6c3462bef6c5
RedCMD <TheRedCMD@gmail.com>
2024-04-10 18:58:45 +1200

    Improve debug Hovers

--- out/HoverProvider.js
+++ out/HoverProvider.js
@@ -5,25 +5,43 @@ const vscode = require("vscode");
 const TreeSitter_1 = require("./TreeSitter");
 exports.HoverProvider = {
     provideHover(document, position, token) {
-        // vscode.window.showInformationMessage(JSON.stringify("Hover"))
-        const tree = (0, TreeSitter_1.getTree)(document);
+        // vscode.window.showInformationMessage(JSON.stringify("Hover"));
+        const trees = (0, TreeSitter_1.getTrees)(document);
+        const jsonTree = trees.jsonTree;
         const point = (0, TreeSitter_1.toPoint)(position);
-        const node = tree.rootNode.descendantForPosition(point);
-        // const node = tree.rootNode.namedDescendantForPosition(point);
+        const node = jsonTree.rootNode.descendantForPosition(point);
+        // const node = jsonTree.rootNode.namedDescendantForPosition(point);
         if (node == null) {
             return;
         }
-        const markdownString = new vscode.MarkdownString();
-        markdownString.appendCodeblock(node.text, 'json-textmate');
-        // const fieldName = node.walk().currentFieldName();
-        // if (fieldName)
-        // 	markdownString.appendText(fieldName + ':');
-        if (node.parent)
-            markdownString.appendText(node.parent.type + ' => ');
-        markdownString.appendText(node.type);
-        const range = (0, TreeSitter_1.toRange)(node);
-        const hover = new vscode.Hover(markdownString, range);
-        // vscode.window.showInformationMessage(JSON.stringify(hover))
-        return hover;
+        if (node.type == 'regex') {
+            const regexTrees = (0, TreeSitter_1.getTrees)(document).regexTrees;
+            const regexTree = regexTrees[node.id];
+            const regexNode = regexTree.rootNode.descendantForPosition(point);
+            const parentNode = regexNode.parent;
+            const markdownString = new vscode.MarkdownString();
+            markdownString.appendText(parentNode.type + ' => ' + regexNode.type);
+            markdownString.appendCodeblock(parentNode.text, 'regex-tmLanguage');
+            markdownString.appendCodeblock(parentNode.toString(), 'scm');
+            const range = (0, TreeSitter_1.toRange)(parentNode);
+            const hover = new vscode.Hover(markdownString, range);
+            // vscode.window.showInformationMessage(JSON.stringify(hover));
+            return hover;
+        }
+        else {
+            const markdownString = new vscode.MarkdownString();
+            markdownString.appendCodeblock(node.text, 'json-textmate');
+            // const fieldName = node.walk().currentFieldName();
+            // if (fieldName)
+            // 	markdownString.appendText(fieldName + ':');
+            if (node.parent)
+                markdownString.appendText(node.parent.type + ' => ');
+            markdownString.appendText(node.type);
+            markdownString.appendCodeblock(node.toString(), 'scm');
+            const range = (0, TreeSitter_1.toRange)(node);
+            const hover = new vscode.Hover(markdownString, range);
+            // vscode.window.showInformationMessage(JSON.stringify(hover));
+            return hover;
+        }
     }
 };
--- src/HoverProvider.ts
+++ src/HoverProvider.ts
@@ -1,30 +1,49 @@
 import * as vscode from 'vscode';
-import { getTree, toPoint, toRange } from "./TreeSitter";
+import { getTrees, toPoint, toRange } from "./TreeSitter";
 
 
-export const HoverProvider = {
+export const HoverProvider: vscode.HoverProvider = {
 	provideHover(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.Hover {
-		// vscode.window.showInformationMessage(JSON.stringify("Hover"))
-		const tree = getTree(document);
+		// vscode.window.showInformationMessage(JSON.stringify("Hover"));
+		const trees = getTrees(document);
+		const jsonTree = trees.jsonTree;
 		const point = toPoint(position);
-		const node = tree.rootNode.descendantForPosition(point);
-		// const node = tree.rootNode.namedDescendantForPosition(point);
+		const node = jsonTree.rootNode.descendantForPosition(point);
+		// const node = jsonTree.rootNode.namedDescendantForPosition(point);
 		if (node == null) {
 			return;
 		}
 
-		const markdownString = new vscode.MarkdownString();
-		markdownString.appendCodeblock(node.text, 'json-textmate');
-		// const fieldName = node.walk().currentFieldName();
-		// if (fieldName)
-		// 	markdownString.appendText(fieldName + ':');
-		if (node.parent)
-			markdownString.appendText(node.parent.type + ' => ');
-		markdownString.appendText(node.type);
+		if (node.type == 'regex') {
+			const regexTrees = getTrees(document).regexTrees;
+			const regexTree = regexTrees[node.id];
+			const regexNode = regexTree.rootNode.descendantForPosition(point);
+			const parentNode = regexNode.parent;
+			const markdownString = new vscode.MarkdownString();
+			markdownString.appendText(parentNode.type + ' => ' + regexNode.type);
+			markdownString.appendCodeblock(parentNode.text, 'regex-tmLanguage');
+			markdownString.appendCodeblock(parentNode.toString(), 'scm');
 
-		const range = toRange(node);
-		const hover = new vscode.Hover(markdownString, range);
-		// vscode.window.showInformationMessage(JSON.stringify(hover))
-		return hover;
+			const range = toRange(parentNode);
+			const hover = new vscode.Hover(markdownString, range);
+			// vscode.window.showInformationMessage(JSON.stringify(hover));
+			return hover;
+		}
+		else {
+			const markdownString = new vscode.MarkdownString();
+			markdownString.appendCodeblock(node.text, 'json-textmate');
+			// const fieldName = node.walk().currentFieldName();
+			// if (fieldName)
+			// 	markdownString.appendText(fieldName + ':');
+			if (node.parent)
+				markdownString.appendText(node.parent.type + ' => ');
+			markdownString.appendText(node.type);
+			markdownString.appendCodeblock(node.toString(), 'scm');
+
+			const range = toRange(node);
+			const hover = new vscode.Hover(markdownString, range);
+			// vscode.window.showInformationMessage(JSON.stringify(hover));
+			return hover;
+		}
 	}
 }
